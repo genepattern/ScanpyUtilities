eb2b04c1ed005522321d4f9b04acfeccf3888bde
Tom Sherman <tomsherman159@gmail.com>
2019-02-08 11:35:50 -0500

    add annotations to genes as well

--- module/add_default_annotations.py
+++ module/add_default_annotations.py
@@ -12,6 +12,11 @@ if 'log_counts' not in adata.obs.keys():
 if 'n_genes' not in adata.obs.keys():
     adata.obs['n_genes'] = (adata.X > 0).sum(1)
 
+if 'n_counts' not in adata.var.keys():
+    adata.var['n_counts'] = adata.X.sum(0)
+if 'n_cells' not in adata.var.keys():
+    adata.var['n_cells'] = (adata.X > 0).sum(0)
+
 print("annotation output:", sys.argv[2])
 adata.write(sys.argv[2], compression='gzip', compression_opts=1)
 
