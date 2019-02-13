4786877645e677bb31c4bfe1ff8983208a3327f2
ted Liefeld <liefeld@broadinstitute.org>
2019-02-13 09:40:29 -0800

    adding in convert_to_h5ad.py script

---
+++ module/convert_to_h5ad.py
@@ -0,0 +1,11 @@
+# datafile, genome, outfilename are the args
+# sys.argv[1]
+
+import sys
+import scanpy.api as sc
+adata = sc.read_10x_h5(sys.argv[1], genome=sys.argv[2])
+adata.write(sys.argv[3], compression='gzip', compression_opts=1)
+
+
+
+
