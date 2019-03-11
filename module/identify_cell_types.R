f483f788fdd569d91288b0342b5ad7785c3609c5
Tom Sherman <tomsherman159@gmail.com>
2019-03-11 18:01:56 -0400

    rough outline for cell type identification

--- module/identify_cell_types.R
+++ module/identify_cell_types.R
@@ -1,1 +1,45 @@
-stop("cell type identification not implemented yet")
+library(Matrix)
+
+# read commnad line arguments
+args <- commandArgs(trailingOnly=TRUE)
+adata_file <- args[1]
+marker_file <- args[2]
+
+# read count data from file
+raw_data <- as.numeric(h5read(adata_file, "/X/data"))
+indices <- as.integer(h5read(adata_file, "/X/indices"))
+indptr <- as.integer(h5read(adata_file, "/X/indptr"))
+counts <- sparseMatrix(i=indices+1, p=indptr, x=raw_data)
+
+# read sample info from file
+obs <- h5read(adata_file, "/obs")
+rownames(obs) <- obs[,1]
+obs <- obs[,-1]
+pd <- AnnotatedDataFrame(data=obs)
+
+# read gene info from file
+var <- h5read(adata_file, "/var")
+rownames(var) <- var[,2]
+var <- var[,-2]
+colnames(var)[1] <- "gene_short_name"
+fd <- new("AnnotatedDataFrame", data=var)
+
+# create cds
+cds <- newCellDataSet(counts, phenoData=pd, featureData=fd,
+    expressionFamily=negbinomial.size())
+cds <- estimateSizeFactors(cds)
+
+# evaluate quality of marker file and return results in an image
+marker_check <- check_markers(cds, marker_file)
+png("ica_marker_check.png")
+plot_markers(marker_check)
+dev.off()
+
+# train classifier and apply to data set
+classifier <- train_cell_classifier(cds=cds, marker_file=marker_file)
+cds <- classify_cells(cds, classifier, cluster_extend=TRUE)
+
+# write cell types back to adata file
+obs$cell_type <- as.character(pd$cell_type)
+h5delete(adata_file, "/obs")
+h5write(obs, adata_file, "/obs")
