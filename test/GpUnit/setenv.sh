3bce651bcb349dca0cbd58a1318af76273a4fc10
ted Liefeld <liefeld@broadinstitute.org>
2019-03-28 13:12:21 -0700

    adding tests

---
+++ test/GpUnit/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export GPUNIT_HOME=/Users/liefeld/GenePattern/gp_dev/GpUnit
+
+#ant -f ${GPUNIT_HOME}/build.xml -Dgpunit.diffStripTrailingCR="--strip-trailing-cr" -Dgp.host="beta.genepattern.org" -Dgp.url="https://cloud.genepattern.org" -Dgp.user="ted-dev" -Dgp.password="" -Dgpunit.testfolder=`pwd` gpunit
+
