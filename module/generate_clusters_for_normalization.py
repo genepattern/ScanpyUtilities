919b8b12aef868fafde78fd486bd6143e308b5bd
Anthony S. Castanza <acastanza@ucsd.edu>
2021-09-22 17:08:34 -0700

    Fix for non-unique names throwing error on object write

--- module/generate_clusters_for_normalization.py
+++ module/generate_clusters_for_normalization.py
@@ -6,6 +6,11 @@ print("normalization input:", sys.argv[1])
 print("clustering cells to use as input for scran")
 
 adata = sc.read(sys.argv[1])
+if len(set(adata.obs_names)) != len(adata.obs_names):
+	adata.obs_names_make_unique()
+
+if len(set(adata.var_names)) != len(adata.var_names):
+	adata.var_names_make_unique()
 
 sc.pp.normalize_per_cell(adata, counts_per_cell_after=1e6)
 sc.pp.log1p(adata)
